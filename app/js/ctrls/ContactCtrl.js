636cb652f5c51f0510963802976a9fd6fc6af7e5
CHANDEL <sanchan3377@gmail.com>
2018-05-03 23:40:32 +0530

    [HOME/CONTACT/ABOUTUS] map still missing...               1.SCROLLTOP is a problem cuz its invoking all animations rapidly.

--- app/js/ctrls/ContactCtrl.js
+++ app/js/ctrls/ContactCtrl.js
@@ -2,6 +2,10 @@
  * Created by Chandel on 19-04-2018.
  */
 app.controller('ContactCtrl', function($scope,$state){
+    $(document).ready(function() {
+        $("html,body").animate({scrollTop: 0}, 0); //0ms for example
+    });
+
     function myMap() {
         var myCenter = new google.maps.LatLng(51.508742,-0.120850);
         var mapCanvas = document.getElementById("map");
@@ -9,9 +13,28 @@ app.controller('ContactCtrl', function($scope,$state){
         var map = new google.maps.Map(mapCanvas, mapOptions);
         var marker = new google.maps.Marker({position:myCenter});
         marker.setMap(map);
-      }
+    }
+
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        var elmnt = document.getElementById("header");
+        var currentScrollPos = window.pageYOffset;
+
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("navbar-fixed-scroll").style.top = "0";
+        } else {
+            document.getElementById("navbar-fixed-scroll").style.top = "-260px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
 
     $scope.contact = function(){
         $state.go("contact")
+    }
+
+    $scope.aboutus = function(){
+        $state.go("aboutus")
     }
 })
--- app/js/ctrls/HomeCtrl.js
+++ app/js/ctrls/HomeCtrl.js
@@ -2,6 +2,9 @@
  * Created by Siddharth on 08-04-2018.
  */
 app.controller('HomeCtrl', function($scope,$state){
+    $(document).ready(function() {
+        $("html,body").animate({scrollTop: 0}, 0); //0ms for example
+    });
 
     var $animation_elements = $('.animation-element');
     var $window = $(window);
@@ -45,7 +48,6 @@ app.controller('HomeCtrl', function($scope,$state){
             }
         });
 
-
         // scroll body to 0px on click
         $('#back-top a').click(function () {
             $('body,html').animate({
@@ -55,19 +57,35 @@ app.controller('HomeCtrl', function($scope,$state){
         });
     });
 
+    var prevScrollpos = window.pageYOffset;
     window.onscroll = function() {scrollFunction()};
 
     function scrollFunction() {
         var elmnt = document.getElementById("header");
-        var y = elmnt.scrollHeight;
+        //var y = elmnt.scrollHeight;
+        var y = 70;
         if (document.body.scrollTop > y || document.documentElement.scrollTop > y) {
             document.getElementById("navbar").style.top = "0";
         } else {
             document.getElementById("navbar").style.top = "-70px";
         }
+
+        var z=260;
+        var currentScrollPos = window.pageYOffset;
+
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("navbar-fixed-scroll").style.top = "0";
+        } else {
+            document.getElementById("navbar-fixed-scroll").style.top = "-260px";
+        }
+        prevScrollpos = currentScrollPos;
     }
 
     $scope.contact = function(){
         $state.go("contact")
+    }
+
+    $scope.aboutus = function(){
+        $state.go("aboutus")
     }
 })
